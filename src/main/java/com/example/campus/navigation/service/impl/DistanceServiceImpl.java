df6f9d78a3c3682764884c86055d28495fae4160
ysxs857 <2911415927@qq.com>
2021-12-07 10:24:42 +0800

    格式化路径“

--- src/main/java/com/example/campus/navigation/service/impl/DistanceServiceImpl.java
+++ src/main/java/com/example/campus/navigation/service/impl/DistanceServiceImpl.java
@@ -12,19 +12,18 @@ import com.example.campus.navigation.service.DistanceService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 @Service
-public class DistanceServiceImpl implements DistanceService {
+public class DistanceServiceImpl implements DistanceService, Comparator<DistanceSearchDO> {
 
     @Resource
     DistanceMapper distanceMapper;
 
     @Resource
     PlaceMapper placeMapper;
+
+    List<DistanceSearchDO> distanceSearchDOList = new ArrayList<>();
 
     @Override
     public DistanceSearchVO Search(DistanceDto distanceDto) {
@@ -38,55 +37,52 @@ public class DistanceServiceImpl implements DistanceService {
         MatGraphClass ma = new MatGraphClass();
 
 
-        for(int i=0;i<distanceDOList.size();i++) {
+        for (int i = 0; i < distanceDOList.size(); i++) {
             ma.insertEdges(distanceDOList.get(i).getPrimaryAddress(),
                     distanceDOList.get(i).getTargetAddress(),
                     distanceDOList.get(i).getValue());
         }
 
 
-        ma.DFS(ma,distanceDto.getPrimaryAddress(), distanceDto.getTargetAddress());
+        ma.DFS(ma, distanceDto.getPrimaryAddress(), distanceDto.getTargetAddress());
 
         List<List> stackList = ma.lists;
 
         DistanceSearchVO distanceSearchVO = new DistanceSearchVO();
 
-        distanceSearchVO.setTotal(stackList.size());
 
-
-        List<DistanceSearchDO> distanceSearchDOList = new ArrayList<>();
-        //
-        for(int i=0;i<stackList.size();i++) {
+        //遍历得到的所有路径数组
+        for (int i = 0; i < stackList.size(); i++) {
             List<String> place = new ArrayList<>();
             Integer sum = 0;
             DistanceSearchDO distanceSearchDO = new DistanceSearchDO();
-            for(int j=0;j<stackList.get(i).size();j++) {
+            for (int j = 0; j < stackList.get(i).size(); j++) {
                 int flag = 1;
-                if(j == stackList.get(i).size()-1) {
+                if (j == stackList.get(i).size() - 1) {
                     flag = 0;
                 }
 
-               int  id1= (int) stackList.get(i).get(j);
-               int  id2 = (int) stackList.get(i).get(j+flag);
+                int id1 = (int) stackList.get(i).get(j);
+                int id2 = (int) stackList.get(i).get(j + flag);
 
-                for(int m =0;m<placeIdNameList.size();m++) {
-                    if(id1 == placeIdNameList.get(m).getId()) {
-                        place.add(placeIdNameList.get(m).getName());
+                for (int m = 0; m < placeIdNameList.size(); m++) {
+                    if (id1 == placeIdNameList.get(m).getId()) {
+                        place.add(placeIdNameList.get(m).getName());//添加路径点的地名
                         break;
                     }
                 }
 
-                for(int n = 0;n<distanceDOList.size();n++) {
+                for (int n = 0; n < distanceDOList.size(); n++) {
 
-                    if((id1 == distanceDOList.get(n).getPrimaryAddress()
-                    &&id2 ==  distanceDOList.get(n).getTargetAddress())
-                    ||(id1 == distanceDOList.get(n).getTargetAddress()
-                            &&id2 ==  distanceDOList.get(n).getPrimaryAddress())
+                    if ((id1 == distanceDOList.get(n).getPrimaryAddress()
+                            && id2 == distanceDOList.get(n).getTargetAddress())
+                            || (id1 == distanceDOList.get(n).getTargetAddress()
+                            && id2 == distanceDOList.get(n).getPrimaryAddress())
                     ) {
 
-                       sum+=distanceDOList.get(n).getValue();
+                        sum += distanceDOList.get(n).getValue();//累加路径权值
 
-                    break;
+                        break;
 
                     }
                 }
@@ -96,10 +92,55 @@ public class DistanceServiceImpl implements DistanceService {
             distanceSearchDO.setPlace(place.toArray(new String[place.size()]));
             distanceSearchDOList.add(distanceSearchDO);
         }
+        //找出最短的几条路径 随机生成数组长度
+        int number = (int) (Math.random() * (6 - 3) + 3);
 
-        distanceSearchVO.setDistanceSearchDOList(distanceSearchDOList);
+        List<DistanceSearchDO> list = new ArrayList<>(number);
+
+        for (int i = 0; i < distanceSearchDOList.size(); i++) {
+
+            for (int j = 0; j < distanceSearchDOList.size() - i - 1; j++) {
+                if (compare(distanceSearchDOList.get(j), distanceSearchDOList.get(j + 1)) > 0) {
+
+                    swap(distanceSearchDOList.get(j), distanceSearchDOList.get(j + 1));
+
+                }
+            }
+        }
+
+        for (int i = 0; i < number; i++) {
+
+            list.add(distanceSearchDOList.get(i));
+
+        }
+
+        distanceSearchVO.setTotal(list.size());
+
+        distanceSearchVO.setDistanceSearchDOList(list);
+
 
         return distanceSearchVO;
+
+
+    }
+
+
+    @Override
+    public int compare(DistanceSearchDO o1, DistanceSearchDO o2) {
+        if (Integer.valueOf(o1.getDistance()) < Integer.valueOf(o2.getDistance())) {
+            return -1;
+        } else if (Integer.valueOf(o1.getDistance()) > Integer.valueOf(o2.getDistance())) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public void swap(DistanceSearchDO o1, DistanceSearchDO o2) {
+
+        DistanceSearchDO temp = o1;
+        o1 = o2;
+        o2 = temp;
 
     }
 
@@ -113,18 +154,20 @@ class MatGraphClass {
     int[][] edges;
     int n, e;
     String[] vexs;
+
     public MatGraphClass() {
         edges = new int[MAXV][MAXV];
-        for(int i=0;i< edges.length;i++) {
-            for(int j=0;j<edges.length;j++) {
-                edges[i][j]=INF;
+        for (int i = 0; i < edges.length; i++) {
+            for (int j = 0; j < edges.length; j++) {
+                edges[i][j] = INF;
             }
         }
         vexs = new String[MAXV];
-        this.n=MAXV;
-        this.e=0;
+        this.n = MAXV;
+        this.e = 0;
     }
-    public void insertEdges(int v1,int v2,int value) {
+
+    public void insertEdges(int v1, int v2, int value) {
         edges[v1][v2] = value;
         edges[v2][v1] = value;
         this.e++;
@@ -154,20 +197,20 @@ class MatGraphClass {
     //创建一个堆栈
 
     Stack<Integer> stack = new Stack<>();
-       List<Integer> list = new ArrayList<>();
-     int[] visited = new int[100];
-     List<List> lists = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
+    int[] visited = new int[100];
+    List<List> lists = new ArrayList<>();
 
-    public  void DFS(MatGraphClass g,int start,int end) {
+    public void DFS(MatGraphClass g, int start, int end) {
         visited[start] = 1; //1代表已经访问过，真值
         stack.push(start);
-        if(start == end) {
-            List<Integer> o =  new ArrayList<Integer>(stack);
-           lists.add(o);
+        if (start == end) {
+            List<Integer> o = new ArrayList<Integer>(stack);
+            lists.add(o);
         }
-        for(int i=1;i<g.n;i++) {
-            if(visited[i]==0&&g.edges[start][i]< g.INF) {
-                DFS(g,i,end);
+        for (int i = 1; i < g.n; i++) {
+            if (visited[i] == 0 && g.edges[start][i] < g.INF) {
+                DFS(g, i, end);
             }
         }
         stack.pop();
@@ -175,6 +218,8 @@ class MatGraphClass {
     }
 
 
-
 }
+
+
+
 
